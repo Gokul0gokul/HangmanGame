1ba3b0b78a6c0f9c4a0a3020ec2d153d9a5f92bb
Gokul0gokul <44828629+Gokul0gokul@users.noreply.github.com>
2024-09-22 13:35:57 +0530

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,35 @@
+import random
+import words
+import stages
+user_list=[]
+guessed_list=[]
+lives=6
+generated_word=random.choice(words.words_list)
+for i in range(len(generated_word)):
+    user_list+='_'
+game_over=False
+while not game_over:
+    char=input("Guess a letter:").lower()
+    if char in guessed_list:
+        print(f"{char} already guessed")
+        continue
+    guessed_list.append(char)
+    for pos in range(len(generated_word)):
+        if generated_word[pos]==char:
+            user_list[pos]=char
+    print(" ".join(user_list))
+    if '_' not in user_list:
+        game_over=True
+        print("HEY YOU WON !!!!!")
+    if char not in generated_word:
+        lives-=1
+        print(stages.stages_list[lives])
+        if lives==0:
+            game_over=True
+            print("YOU LOST !!!")
+            print(f"WORD : {generated_word}")
+
+
+
+
+
---
+++ words.py
@@ -0,0 +1,2 @@
+words_list=['aeroplane','crying',"minute","consider","evident","practice","utter","conduct","elephant"]
+str="Hello"
